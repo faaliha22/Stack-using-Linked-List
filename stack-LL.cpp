d34ddebed258db51b773f60cd857d03d7490b0a8
faaliha22 <faaliha.m2025cse@sece.ac.in>
2026-02-12 12:15:19 +0530

    Add files via upload

---
+++ stack-LL.cpp
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//structure
+struct node{
+	int data;
+	struct node *next;
+};
+
+struct node *top = NULL;
+
+//push
+void push(int value)
+{   
+    
+    
+	struct node *nn;
+	nn = (struct node*)malloc(sizeof(struct node));
+	nn->data = value;
+	nn->next = top;
+	top = nn;
+	printf("%d pushed\n",nn->data);
+}
+
+void pop() 
+{   if(top == NULL)
+    printf("Stack is underflowing!!");
+    
+	struct node *temp;
+	temp = top;
+	printf("%d deleted\n",top->data);
+	top = top->next;
+	free(temp);
+
+}
+
+
+void peek()
+{   if(top == NULL)
+    printf("Stack is empty!!");
+    
+	struct node *temp;
+	temp = top;
+	
+	printf("%d is the top value\n",temp->data);
+}
+
+void display()
+{	 if(top == NULL)
+    printf("Stack is empty!!");
+    
+	struct node *temp;
+	temp = top;
+	printf("----displaying----\n");
+	while(temp!= NULL)
+	{
+		printf("%d\n",temp->data);
+		temp = temp->next;
+	}
+}
+
+int main()
+{   
+    printf("---MENU---\n");
+    printf("\n1.Push\n2.Pop\n3.Peek\n4.Display\n5.Exit\n");
+
+ int choice;
+ 
+do{
+    printf("Enter choice : ");
+ scanf("%d",&choice);
+ 
+ switch(choice)
+ {
+ case 1:
+ {
+ 	printf("Enter value to insert : ");
+ 	int v;
+ 	scanf("%d",&v);
+ 	push(v);
+    break;
+}
+
+case 2:
+	pop();
+	break;
+	
+case 3:
+peek();
+break;
+
+case 4:
+display();
+break;
+
+case 5:
+return 0;
+break;
+
+default:
+	printf("invalid");
+	break;
+	
+	
+}
+}while(choice != 5);
+  
+
+}
